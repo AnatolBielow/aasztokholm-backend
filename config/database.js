a2805b85565e9fab10f4e14d40ea467fc8b666b9
Anatol Bielow <84202274+AnatolBielow@users.noreply.github.com>
2022-11-17 22:20:44 +0100

    first commit

--- config/database.js
+++ config/database.js
@@ -1,59 +1,76 @@
-const path = require("path");
-const parse = require("pg-connection-string").parse;
+// const path = require("path");
+// const parse = require("pg-connection-string").parse;
 
-module.exports = ({ env }) => {
-  if (env("NODE_ENV") === "production") {
-    const config = parse(process.env.DATABASE_URL);
-    return {
-      defaultConnection: "default",
-      connection: {
-        default: {
-          connector: "bookshelf",
-          settings: {
-            client: "postgres",
-            host: config.host,
-            port: config.port,
-            database: config.database,
-            username: config.user,
-            password: config.password,
-          },
-          options: {
-            ssl: false,
-          },
-        },
-      },
-    };
-  }
-  return {
-    connection: {
-      client: "sqlite",
-      connection: {
-        filename: path.join(
-          __dirname,
-          "..",
-          env("DATABASE_FILENAME", ".tmp/data.db")
-        ),
-      },
-      useNullAsDefault: true,
-    },
-  };
-};
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: 'mysql',
+// module.exports = ({ env }) => {
+//   if (env("NODE_ENV") === "production") {
+//     // const config = parse(process.env.DATABASE_URL);
+//     // return {
+//     //   defaultConnection: "default",
+//     //   connection: {
+//     //     default: {
+//     //       connector: "bookshelf",
+//     //       settings: {
+//     //         client: "postgres",
+//     //         host: config.host, 
+//     //         port: config.port,
+//     //         database: config.database,
+//     //         username: config.user,
+//     //         password: config.password,
+//     //       },
+//     //       options: {
+//     //         ssl: false,
+//     //       },
+//     //     },
+//     //   },
+//     // };
+
+//     return {
+//       connection: {
+//             client: 'mysql',
+//             connection: {
+//               host: env('DATABASE_HOST', 'localhost'),
+//               port: env.int('DATABASE_PORT', 8889),
+//               database: env('DATABASE_NAME', 'strapi'),
+//               user: env('DATABASE_USERNAME', 'root'),
+//               password: env('DATABASE_PASSWORD', 'root'),
+//               ssl: {
+//                 rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+//               },
+//             },
+//             debug: false,
+//           },
+//     }
+//   }
+//   return {
 //     connection: {
-//       host: env('DATABASE_HOST', 'localhost'),
-//       port: env.int('DATABASE_PORT', 8889),
-//       database: env('DATABASE_NAME', 'strapi'),
-//       user: env('DATABASE_USERNAME', 'root'),
-//       password: env('DATABASE_PASSWORD', 'root'),
-//       ssl: {
-//         rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+//       client: "sqlite",
+//       connection: {
+//         filename: path.join(
+//           __dirname,
+//           "..",
+//           env("DATABASE_FILENAME", ".tmp/data.db")
+//         ),
 //       },
+//       useNullAsDefault: true,
 //     },
-//     debug: false,
-//   },
-// });
+//   };
+// };
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'mysql',
+    connection: {
+      host: env('DATABASE_HOST', 'localhost'),
+      port: env.int('DATABASE_PORT', 8889),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'root'),
+      password: env('DATABASE_PASSWORD', 'root'),
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+      },
+    },
+    debug: false,
+  },
+});
 
  
 
